d21f29480981b57ad3fa6e9041681544838616bd
hihihioli <olivermandel123@gmail.com>
2025-12-09 13:34:57 -0500

    4K render-only mode

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,8 @@ fn F(x: f64, y: f64) -> f64 {
 }
 #[macroquad::main(window_conf)] //pass with config in
 async fn main() { //todo: fix coordinate system
-    let (width, height) = (screen_width() as f64, screen_height() as f64);
-    let mut scale = 20f64;
+    let (width, height) = (3840f64, 2160f64);
+    let mut scale = 150f64;
     let mut corner1: (f64, f64) = (-1f64 * width / (2f64 * scale), height / (2f64 * scale));
     let mut corner2: (f64, f64) = ( width / (2f64 * scale), -1f64 * height / (2f64 * scale));
     let dpi_scale = screen_dpi_scale() as f64;
@@ -45,47 +45,8 @@ async fn main() { //todo: fix coordinate system
         height as u16 * dpi_scale as u16,
         BLACK,
     ); //an image to manipulate
-    let mut tex = Texture2D::from_image(&img);
-
-    loop {
-        //set_fullscreen(true); //force into fullscreen; not necessary
-        //handle zoom
-        let delta_time = get_frame_time() as f64;
-        scale += scale * mouse_wheel().1 as f64 * delta_time * 0.5;
-        if scale < 1f64 {
-            scale = 1f64;
-        }
-        //handle movement
-        if is_mouse_button_down(MouseButton::Left) { //mouse movements
-            let mouse_delta = mouse_delta_position();
-            x_shift+=mouse_delta.x as f64 /scale * width / 2.0;
-            y_shift+=mouse_delta.y as f64 / scale * height / 2.0;
-        }
-        if is_key_down(KeyCode::Right) {
-            x_shift += 200f64 * delta_time / scale;
-        }
-        if is_key_down(KeyCode::Left) {
-            x_shift -= 200f64 * delta_time / scale;
-        }
-        if is_key_down(KeyCode::Down) {
-            y_shift += 200f64 * delta_time / scale;
-        }
-        if is_key_down(KeyCode::Up) {
-            y_shift -= 200f64 * delta_time / scale;
-        }
-
-        //update corners
-        corner1 = (
-            -1f64 * width / (2f64 * scale) + x_shift,
-            height / (2f64 * scale) - y_shift,
-        );
-        corner2 = (
-            //
-            width/ (2f64 * scale) + x_shift,
-            -1f64 * height / (2f64 * scale) - y_shift,
-        );
-        let x_scale = (width / scale) / (corner2.0 - corner1.0);
-        let y_scale = (height / scale) / (corner1.1 - corner2.1);
+    
+        
 
         // let dx = (corner2.0 - corner1.0) / width; // math units per screen pixel in x
         // let dy = (corner2.1 - corner1.1) / height; // math units per screen pixel in y
@@ -139,92 +100,12 @@ async fn main() { //todo: fix coordinate system
                 //}
             }
         }
-        tex.update(&img);
-        draw_texture_ex(
-            &tex,
-            0f32,
-            0f32,
-            WHITE,
-            DrawTextureParams {
-                dest_size: Some(vec2(width as f32, height as f32)),
-                ..Default::default()
-            },
-        );
+        img.export_png("image.png");
+        
 
-        //Draw the axes
-        let x_middle = (-1f64 * corner1.0) * scale * x_scale;
-        let y_middle = (corner1.1) * scale * y_scale;
-
-        //y-axis
-        draw_line(
-            x_middle as f32,
-            0f32,
-            x_middle as f32,
-            height as f32,
-            1f32,
-            GREEN,
-        );
-        //draw the tick marks
-        for i in 0..((height - y_middle) / (scale * y_scale) + 1f64) as i32 {
-            //down
-            draw_line(
-                (x_middle + 3f64) as f32,
-                (i as f64 * scale * y_scale + y_middle) as f32,
-                x_middle as f32 - 3f32,
-                (i as f64 * scale * y_scale + y_middle) as f32,
-                1f32,
-                GREEN,
-            );
-        }
-        for i in 0..((y_middle + 1f64) / (scale * y_scale) + 1f64) as i32 {
-            //up
-            draw_line(
-                (x_middle + 3f64) as f32,
-                (-1f64 * i as f64 * y_scale * scale + y_middle) as f32,
-                (x_middle - 3f64) as f32,
-                (-1f64 * i as f64 * y_scale * scale + y_middle) as f32,
-                1f32,
-                GREEN,
-            );
-        }
-
-        //x-axis
-        draw_line(
-            0f32,
-            y_middle as f32,
-            width as f32,
-            y_middle as f32,
-            1f32,
-            GREEN,
-        ); //x-axis
-        //draw the tick marks
-        for i in 0..((width - x_middle) / (scale * x_scale) + 1f64) as i32 {
-            //right
-            draw_line(
-                (i as f64 * scale * x_scale + x_middle) as f32,
-                (y_middle + 3f64) as f32,
-                (i as f64 * scale * x_scale + x_middle) as f32,
-                (y_middle - 3f64) as f32,
-                1f32,
-                GREEN,
-            );
-        }
-        for i in 0..((width + x_middle) / (scale * x_scale) + 1f64) as i32 {
-            //left
-            draw_line(
-                (-1f64 * i as f64 * scale * x_scale + x_middle) as f32,
-                 (y_middle + 3f64) as f32,
-                  (-1f64 * i as f64 * scale * x_scale + x_middle) as f32,
-                   (y_middle - 3f64) as f32,
-                1f32,
-                GREEN,
-            );
-        }
-
-        draw_text_in_corner(&corner1, &corner2, &scale); //draw text
 
         next_frame().await //draw the frame I think
-    }
+    
 }
 
 
